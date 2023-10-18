cf0ce75cf7f2a6314799fbf83af73c1f3893dffd
Nox <gabrismar333@gmail.com>
2023-10-18 17:23:18 -0400

    "Consoles Page, Filter consoles by name"
    Add: "src/UI/api/Console.js" - Created class 'Console';
    
    Add: "src/UI/components/data/console_all.js" - Created component to show all consoles;
    
    Add: "src/UI/components/data/console_search.js" - Created component to filter consoles by name;
    
    Update: "src/UI/components/data/index.js" - Added condition to load 'console_all' and 'console_search' components;
    
    Add: "src/pages/consoles.html" - Created consoles page;

---
+++ src/UI/api/Console.js
@@ -0,0 +1,22 @@
+import HOST from "../config.js"
+
+const endpoint = HOST + "consoles"
+
+export default class Console {
+    static async getAll() {
+        const res1 = await fetch(`${endpoint}/`)
+        const { data } = await res1.json()
+        data.forEach(console => {
+            console["img"] = `${HOST}images/${console["img"]}`
+        })
+        return data
+    }
+    static async getByName(name) {
+        const res = await fetch(`${endpoint}/name/${name}`)
+        const { data } = await res.json()
+        data.forEach(console => {
+            console["img"] = `${HOST}images/${console["img"]}`
+        })
+        return data
+    }
+}

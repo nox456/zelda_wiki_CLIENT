4249a0091e68a5ab6f3de9f9d2e81859f43d732f
Nox <gabrismar333@gmail.com>
2023-10-29 12:24:44 -0400

    "Sidebar Responsive, develop_by Component"
    Update: "src/assets/css/input.css" - Placed main container in row 2;
    
    Update: "src/assets/js/ui/components/develop_by.js" - Changed some styles of develop_by component;
    
    Update: "src/assets/js/ui/components/link.js" - Increased width of link button;
    
    Update: "src/assets/js/ui/components/page_icon.js" - Reduced size of page icon;
    
    Update: "src/assets/js/ui/components/sidebar.js" - Placed sidebar in top of screen using grid;
                                                       Changed layout of develop_by component in sidebar;
    
    Update: "src/index.html" - Changed layout of main text in landing page;
    
    Update: "src/pages/categories.html" - Used grid to place the sidebar at the top and main container on the rest of the screen;

--- src/assets/js/ui/components/link.js
+++ src/assets/js/ui/components/link.js
@@ -1,6 +1,6 @@
 export default function link() {
     document.querySelector("#link").outerHTML = `
         <a href="/pages/categories.html"
-           class="text-white border border-green-300 hover:bg-green-300 w-2/5 sm:w-3/5 py-2 text-center rounded-lg transition-colors cursor-pointer hover:text-black shadow-black shadow-lg font-bold">Categorias</a>
+           class="text-white border border-green-300 hover:bg-green-300 w-2/5 sm:w-4/5 py-2 text-center rounded-lg transition-colors cursor-pointer hover:text-black shadow-black shadow-lg font-bold">Categorias</a>
     `
 }

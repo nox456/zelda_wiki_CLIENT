bd70cff83c5f47b2df77b80d585eab609f1d3055
Nox <gabrismar333@gmail.com>
2023-11-01 22:36:15 -0400

    "Article elements, Removed some breakpoints"
    Update: "src/assets/js/ui/components/category_article.js" - Changed article element height;
    
    Update: "src/assets/js/ui/components/develop_by.js" - Added ":";
    
    Update: "src/pages/categories.html" - Removed breakpoints of some styles to use it in all screens;

--- src/assets/js/ui/components/category_article.js
+++ src/assets/js/ui/components/category_article.js
@@ -6,7 +6,7 @@ export default function category_article() {
         const description = element.querySelector("#description").innerHTML
         const linkUrl = element.querySelector("#linkUrl").innerHTML
         element.outerHTML = `
-            <article class="rounded-md border-4 border-blue-300 text-white w-52 sm:w-[45%] sm:h-[55%] h-1/2 overflow-hidden cursor-pointer bg-slate-700 group relative">
+            <article class="rounded-md border-4 border-blue-300 text-white w-52 sm:w-[45%] sm:h-[55%] h-[70%] overflow-hidden cursor-pointer bg-slate-700 group relative">
                 <a href="/pages/${linkUrl}" class="bg-black bg-opacity-60 absolute top-0 left-0 w-full h-full opacity-0 group-hover:opacity-100 sm:group-hover:opacity-0 transition-opacity duration-[30ms] text-4xl font-zelda flex items-center justify-center">${title}</a>
                 <img src="/assets/img/categories/${imgUrl}" class="h-1/2 object-cover group-hover:h-full sm:group-hover:h-1/2 transition-[height] duration-[30ms]"/>
                 <hr class="border border-blue-300"/>
--- src/assets/js/ui/components/develop_by.js
+++ src/assets/js/ui/components/develop_by.js
@@ -1,7 +1,7 @@
 export default function develop_by() {
     document.querySelector("#develop-by").outerHTML = `
         <div class="w-fit flex flex-col items-center gap-2 sm:gap-0 justify-end sm:justify-start sm:pt-2">
-            <p class="font-bold text-white px-2 text-center">Desarrollador</p>
+            <p class="font-bold text-white px-2 text-center">Desarrollador:</p>
             <a href="https://github.com/nox456" target="_blank" class="flex items-center border-t border-r border-green-300 transition-colors gap-2 hover:bg-green-300 hover:bg-opacity-30 p-2 text-white w-full rounded-tr-md justify-center sm:h-full">
                 <img src="/assets/img/avatar.jpg" class="rounded-full w-10 h-10 sm:w-7 sm:h-7 object-cover"/>
                 <span class="font-bold sm:text-sm">Nox</span>

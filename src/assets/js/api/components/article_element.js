253d6bcb84a9ec7201ba94c4ea95add216b06b5f
Nox <gabrismar333@gmail.com>
2023-11-03 00:03:13 -0400

    "Links of games articles,Short nes and snes names"
    Update: "src/assets/js/api/components/article_element.js" - Short long nes and snes names;
    
    Update: "src/assets/js/api/game/Game.js" - Get 'tableName' from data fetched;
    
    Update: "src/assets/js/api/game/game_all.js" - Added 'tableName' to 'article_element' function;
    
    Update: "src/assets/js/api/game/game_search.js" - Added 'tableName' to 'article_element' function;

--- src/assets/js/api/components/article_element.js
+++ src/assets/js/api/components/article_element.js
@@ -1,16 +1,22 @@
-export default function article_element(articles,container, table) {
-    articles.forEach(article => {
-        const element = document.createElement("a")
-        const file_name = article.name.replace("The Legend of Zelda - ","").replace(/ /gi,"_").toLowerCase()
-        element.className = "border border-blue-300 rounded-md overflow-hidden w-56 h-52 bg-slate-700 cursor-pointer group"
-        element.href = `/pages/${table}/${file_name}.html`
+export default function article_element(articles, container, table) {
+    articles.forEach((article) => {
+        const element = document.createElement("a");
+        const file_name = article.name
+            .replace("The Legend of Zelda - ", "")
+            .replace("Nintendo Entertainment System (NES)","nes")
+            .replace("Super Nintendo Entertainment System (SNES)","snes")
+            .replace(/ /gi, "_")
+            .toLowerCase();
+        element.className =
+            "border border-blue-300 rounded-md overflow-hidden w-56 h-52 bg-slate-700 cursor-pointer group";
+        element.href = `/pages/${table}/${file_name}.html`;
         element.innerHTML = `
             <h1 class="text-xl text-center font-zelda text-white group-hover:bg-blue-300 transition-colors group-hover:text-black">${article.name}</h1>
             <hr class="border border-blue-300"/>
             <div class="flex justify-center items-center h-full overflow-hidden">
                 <img src="${article.img}" class="object-cover h-full"/>
             </div>
-        `
-        container.appendChild(element)
-    })
+        `;
+        container.appendChild(element);
+    });
 }
--- src/assets/js/api/game/Game.js
+++ src/assets/js/api/game/Game.js
@@ -1,31 +1,31 @@
-import HOST from "../../config.js"
-const endpoint = HOST + "games/"
+import HOST from "../../config.js";
+const endpoint = HOST + "games/";
 
 export default class Game {
     static async getAll() {
-        const res = await fetch(endpoint)
-        const { data } = await res.json()
-        data.forEach(game => {
-            game["img"] = HOST + "images/" + game["img"]
-        })
-        return data
+        const res = await fetch(endpoint);
+        const { data, tableName } = await res.json();
+        data.forEach((game) => {
+            game["img"] = HOST + "images/" + game["img"];
+        });
+        return { data, tableName };
     }
     static async getByName(name) {
-        const res = await fetch(endpoint + `name/${name}`)
-        const { data } = await res.json()
-        data.forEach(game => {
-            game["img"] = HOST + "images/" + game["img"]
-        })
-        return data
+        const res = await fetch(endpoint + `name/${name}`);
+        const { data, tableName } = await res.json();
+        data.forEach((game) => {
+            game["img"] = HOST + "images/" + game["img"];
+        });
+        return { data, tableName };
     }
     static async getByConsole(console) {
-        const res1 = await fetch(HOST + `consoles/name/${console}`)
-        const { data } = await res1.json()
-        const res2 = await fetch(endpoint + `console/${data[0].id}`)
-        const games = await res2.json()
-        games.data.forEach(game => {
-            game["img"] = HOST + "images/" + game["img"]
-        })
-        return games.data
+        const res1 = await fetch(HOST + `consoles/name/${console}`);
+        const { data } = await res1.json();
+        const res2 = await fetch(endpoint + `console/${data[0].id}`);
+        const games = await res2.json();
+        games.data.forEach((game) => {
+            game["img"] = HOST + "images/" + game["img"];
+        });
+        return { data: games.data, tableName: games.tableName };
     }
 }

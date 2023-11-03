8fe39f7765abf46a870cacc744f0b717b29d986e
Nox <gabrismar333@gmail.com>
2023-11-02 23:09:22 -0400

    "Articles pages names"
    Update: "src/assets/js/api/components/article_element.js" - Remove 'The Legend of Zelda - ' prefix of games titles in articles pages names;
                                                                Convert in lower case articles pages names;

--- src/assets/js/api/components/article_element.js
+++ src/assets/js/api/components/article_element.js
@@ -1,7 +1,7 @@
 export default function article_element(articles,container, table) {
     articles.forEach(article => {
         const element = document.createElement("a")
-        const file_name = article.name.replace(/ /gi,"_")
+        const file_name = article.name.replace("The Legend of Zelda - ","").replace(/ /gi,"_").toLowerCase()
         element.className = "border border-blue-300 rounded-md overflow-hidden w-56 h-52 bg-slate-700 cursor-pointer group"
         element.href = `/pages/${table}/${file_name}.html`
         element.innerHTML = `

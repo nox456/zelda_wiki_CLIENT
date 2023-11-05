296dee57aff9c6acbe00872e64bc9af3ae3a73d4
Nox <gabrismar333@gmail.com>
2023-11-05 10:47:11 -0400

    "Din, Nayru and Farore Pages, Some titles pages"
    Update: "src/assets/js/api/components/article_img.js" - Added 'object fit' style;
    
    Update: "src/pages/characters/agahnim.html" - Changed title page;
    
    Add: "src/pages/characters/din.html" - Din Page;
    
    Add: "src/pages/characters/farore.html" - Farore Page;
    
    Update: "src/pages/characters/marín.html" - Changed title page;
    
    Add: "src/pages/characters/nayru.html" - Nayru Page;
    
    Update: "src/pages/characters/sheik.html" - Changed title page;

--- src/assets/js/api/components/article_img.js
+++ src/assets/js/api/components/article_img.js
@@ -1,7 +1,7 @@
 export default function article_img(articleImg,element) {
     element.outerHTML = `
         <div class="flex justify-center py-2 h-[40%]">
-            <img src="${articleImg}"/>
+            <img src="${articleImg}" class="object-cover"/>
         </div>
     `
 }

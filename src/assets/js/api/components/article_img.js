70911f001545001e56b6603cb8b81ffe57a4a245
Nox <gabrismar333@gmail.com>
2023-11-05 10:18:40 -0400

    "Navi, Marín, Agahnim and Sheik articles pages"
    Update: "src/assets/js/api/components/article_img.js" - Added fixed height of image;
    
    Add: "src/pages/characters/agahnim.html" - Agahnim Page;
    
    Add: "src/pages/characters/marín.html" - Marín Page;
    
    Add: "src/pages/characters/navi.html" - Navi Page;
    
    Add: "src/pages/characters/sheik.html" - Sheik Page;

--- src/assets/js/api/components/article_img.js
+++ src/assets/js/api/components/article_img.js
@@ -1,6 +1,6 @@
 export default function article_img(articleImg,element) {
     element.outerHTML = `
-        <div class="flex justify-center py-2">
+        <div class="flex justify-center py-2 h-[40%]">
             <img src="${articleImg}"/>
         </div>
     `

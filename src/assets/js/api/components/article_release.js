05f30e9aef3d74e11b620644b28e7aad5ecad23f
Nox <gabrismar333@gmail.com>
2023-11-09 22:32:17 -0400

    "NES Page, Formated release date"
    Update: "src/assets/js/api/components/article_release.js" - Formated release date;
    
    Add: "src/pages/consoles/nes.html" - NES Page;

--- src/assets/js/api/components/article_release.js
+++ src/assets/js/api/components/article_release.js
@@ -1,6 +1,21 @@
 export default function article_release(articleRelease) {
+    const months = {
+        "1": "Enero",
+        "2": "Febrero",
+        "3": "Marzo",
+        "4": "Abril",
+        "5": "Mayo",
+        "6": "Junio",
+        "7": "Julio",
+        "8": "Septiembre",
+        "9": "Agosto",
+        "10": "Octubre",
+        "11": "Noviembre",
+        "12": "Diciembre"
+    }
     const releaseElement = document.querySelector("#article-release")
+    const release = new Date(articleRelease)
     releaseElement.outerHTML = `
-        <p>${articleRelease}</p>
+        <p>${release.getDate()} - ${months[release.getMonth()]} - ${release.getFullYear()}</p>
     `
 }
